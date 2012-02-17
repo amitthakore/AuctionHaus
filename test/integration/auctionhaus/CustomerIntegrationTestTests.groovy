39f44a028316e9e5742823d5e211bf16832e6e72
amitthakore <amitthakore16@gmail.com>
2012-02-17 01:24:22 -0600

    Updated Customer Integration Test

--- test/integration/auctionhaus/CustomerIntegrationTestTests.groovy
+++ test/integration/auctionhaus/CustomerIntegrationTestTests.groovy
@@ -22,26 +22,39 @@ class CustomerIntegrationTestTests extends GroovyTestCase   {
     void testUniqueEmailError(){
 
   //    C-2: Email address must be a unique field (integration test)
-   def customer = new Customer(email:"amitthakore16@gmail.com",password: "123456",createdDate: new Date())
-        customer.save(flush:true)
-   
-    customer.email == "amitthakore16@gmail.com"
+        def customer = new Customer()
+        customer.email = "amitthakore16@gmail.com"
+        customer.password = "123459"
+        customer.createdDate  = new Date()
+        customer.save()
+        def customer1 = new Customer()
+        customer1.email = "amitthakore16@gmail.com"
+        customer1.password = "123459"
+        customer1.createdDate  = new Date()
+        customer1.save()
 
-       !customer.validate()
-        assert 'unique' != customer.errors["email"]
+        assert  !customer1.validate()
+        assert customer1.errors["email"] != null
     }
 
     @Test
     void testUniqueEmailOK(){
 
         //    C-2: Email address must be a unique field (integration test)
-        def customer = new Customer(email:"amitthakore16@gmail.com",password: "123456",createdDate: new Date())
-        customer.save(flush:true)
+        def customer = new Customer()
+        customer.email = "amitthakore16@gmail.com"
+        customer.password = "123459"
+        customer.createdDate  = new Date()
+        customer.save()
+        def customer1 = new Customer()
+        customer1.email = "amitthakore17@gmail.com"
+        customer1.password = "123459"
+        customer1.createdDate  = new Date()
+        customer1.save()
 
-        customer.email == "amitthakore17@gmail.com"
 
-        customer.validate()
-        assert 'unique' == customer.errors["email"]
+       assert customer1.validate()
+        assert null == customer1.errors["email"]
     }
 
 }

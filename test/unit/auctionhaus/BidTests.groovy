51aa8c78acf69062751a38edb4e43e76435acbc0
Hennepin County Mobile Macbook 1 <hcmobile1@Hennepins-MacBook-Pro.local>
2012-02-16 19:36:06 -0600

    added bidtest b2

--- test/unit/auctionhaus/BidTests.groovy
+++ test/unit/auctionhaus/BidTests.groovy
@@ -13,8 +13,9 @@ class BidTests {
 
 
    // B-1: Bids have the following required fields: amount and date/time of bid (unit test)
+    //check that null required bid fields cause an error (exceptional case)
     void testBidNullFieldsCauseError()
-    {                                                                                                                                                 mockForConstraintsTests(Customer)
+    {
         mockForConstraintsTests(Bid)
         def bidNull = new Bid()
 
@@ -27,8 +28,9 @@ class BidTests {
     }
 
     // B-1: Bids have the following required fields: amount and date/time of bid (unit test)
+    //check that there is no nullable error when the required fields are present (happy path)
     void testBidNonNullFieldsOK()
-    {                                                                                                                                                 mockForConstraintsTests(Customer)
+    {
         mockForConstraintsTests(Bid)
         def bidTest = new Bid()
 
@@ -43,10 +45,40 @@ class BidTests {
 
     }
 
+    //B-2: Bids are required to be for a Listing (unit test)
+    //check that null required listing causes an error (exceptional case)
+    void testBidListingNullCausesError()
+    {
+
+        mockForConstraintsTests(Bid)
+        def bidNull = new Bid()
+
+        assert  !bidNull.validate()
+
+
+        assert "nullable" == bidNull.errors["listing"]
+    }
+
+    //B-2: Bids are required to be for a Listing (unit test)
+    //check that no nullable error when listing is present (exceptional case)
+    void testBidListingNonNullOK()
+    {
+        mockForConstraintsTests(Bid)
+        def bidTest = new Bid()
+
+        def bidder = new Customer(email:"amit_thakore1@yahoo.com",password: "1234567",createddate: new Date())
+        def listingTest = new Listing(listingName: "Apple TV",listingEndDateTime: new Date(),StartingBidPrice: 10.00, seller:bidder)
+        bidTest.listing = listingTest
+        bidTest.validate()
+
+
+        assert "nullable" != bidTest.errors["listing"]
+
+    }
 
   //  B-3: Bids are required to have a bidder (Customer) (unit test)
-    
-    void testBidListingNullCausesError()
+  //check that null required bidder causes an error (exceptional case)
+    void testBidBidderNullCausesError()
     {
         mockForConstraintsTests(Bid)
         def bidNull = new Bid()
@@ -62,13 +94,16 @@ class BidTests {
     }
 
     //  B-3: Bids are required to have a bidder (Customer) (unit test)
-
-    void testBidListingNonNullOK()
+    //check that no nullable error when the bidder is present (happy path)
+    void testBidBidderNonNullOK()
     {
         mockForConstraintsTests(Bid)
         def bidTest = new Bid()
-        
+
+
         bidTest.bidder = new Customer(email:"amit_thakore1@yahoo.com",password: "1234567",createddate: new Date())
+
+
 
         bidTest.validate()
 
